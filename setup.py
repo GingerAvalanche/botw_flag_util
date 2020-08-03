430ccbe57e65f52e34b1ef132b686066c262303d
GingerAvalanche <chodness@gmail.com>
2020-08-03 13:29:01 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="botw_flag_util",
-    version="0.0.1",
+    version="0.0.2",
     author="Ginger",
     author_email="chodness@gmail.com",
     description="Game data and save game data flag utilities for LoZ:BotW",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/GingerAvalanche/botw_flag_util",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.74",
+    python_requires=">=3.7.4",
 )
