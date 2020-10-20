80a94aaeb5d5ac733064dc874380bcfb2a6f3b0b
GingerAvalanche <chodness@gmail.com>
2020-10-20 06:20:38 -0700

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="botw_flag_util",
-    version="0.2.9",
+    version="0.2.10",
     author="Ginger",
     author_email="chodness@gmail.com",
     description="Game data and save game data flag utilities for LoZ:BotW",
